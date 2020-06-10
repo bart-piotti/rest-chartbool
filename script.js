96e7ee962d5c04b05a424e9e52f70c4408403d9a
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-06-10 19:53:36 +0200

    Milestone I e III

--- script.js
+++ script.js
@@ -15,6 +15,12 @@ $(document).ready(function() {
         'December': 0
     };
 
+    var quarters = {
+        Q1: 0,
+        Q2: 0,
+        Q3: 0,
+        Q4: 0
+    }
 
     var guadagni_a_testa = {};
     var guadagno_annuale = 0;
@@ -31,11 +37,10 @@ $(document).ready(function() {
 
                 //LINE CHART
                 mese_data_corrente = moment(data[i].date, 'DD/MM/YYYY').format('MMMM')
-                console.log(mese_data_corrente);
+                // console.log(mese_data_corrente + moment(data[i].date, 'DD/MM/YYYY').format('Q'));
+                // console.log(data[i].amount);
 
                 months[mese_data_corrente] += data[i].amount;
-
-
 
                 //PIE CHART
                 //Crea oggetto {Salesman: guadagno}
@@ -47,6 +52,9 @@ $(document).ready(function() {
 
                 //Somma tutti i guadagni per il totale annuale
                 guadagno_annuale += data[i].amount
+
+                //Riempie l'oggetto 'quarters' con il guadagno di ciascun quadrimestre
+                quarters['Q' + moment(data[i].date, 'DD/MM/YYYY').format('Q')] += data[i].amount;
             }
 
             //Trasforma in percentuale i valori del guadagno a testa
@@ -56,6 +64,7 @@ $(document).ready(function() {
 
             lineChart(Object.keys(months), Object.values(months))
             pieChart(Object.keys(guadagni_a_testa), Object.values(guadagni_a_testa))
+            barChart(Object.keys(quarters), Object.values(quarters))
 
         }
     })
@@ -72,11 +81,21 @@ $(document).ready(function() {
                     label: 'Guadagno azienda (€)',
                     data: y,
                     fill: true,
-                    borderColor: 'rgb(60,179,113)',
-                    backgroundColor: 'rgba(60,179,113,.5)',
+                    borderColor: 'rgb(250, 204, 0)',
+                    backgroundColor: 'rgba(250, 204, 0,.15)',
                     lineTension: 0,
                 }],
             },//Data
+            options: {
+                title: {
+                    display: true,
+                    text: 'Andamento annuale azienda',
+                    position: 'left',
+                    fontSize: 18,
+                    padding: 50,
+                    fontStyle: 'normal',
+                }
+            }
         }); //config
     }
 
@@ -98,13 +117,46 @@ $(document).ready(function() {
             },//Data
             options: {
                     legend: {
-                        position: 'left'
+                        position: 'left',
                     },
                     title: {
                         display: true,
-                        text: '% per salesman',
+                        text: '% Guadagni per impiegato',
                         position: 'left',
-                        fontSize: 30,
+                        fontSize: 18,
+                        padding: 50,
+                        fontStyle: 'normal',
+                    }
+            }
+        }); //config
+    }
+
+    //Grafico 3
+    var bar_chart = document.getElementById('barChart').getContext('2d');
+    function barChart(x, y) {
+        var config = new Chart(bar_chart, {
+            type: 'bar',
+            data: {
+                labels: x,
+                datasets: [{
+                    data: y,
+                    fill: true,
+                    backgroundColor: ['rgba(255, 173, 173, .45)', 'rgba(255, 214, 165, .45)', 'rgba(253, 255, 182, .45)', 'rgba(202, 255, 191, .6)'],
+                    borderColor: ['rgba(255, 173, 173, 1)', 'rgba(255, 214, 165, 1)', 'rgba(253, 255, 182, 1)', 'rgba(202, 255, 191, 1)'],
+                    borderWidth: 2,
+
+                }],
+            },//Data
+            options: {
+                    legend: {
+                        display: false,
+                    },
+                    title: {
+                        display: true,
+                        text: 'Guadagno per quadrimestre (€)',
+                        position: 'left',
+                        fontSize: 18,
+                        padding: 50,
                         fontStyle: 'normal',
                     }
             }
