3f0ddbfd77cc0f77230fc07f19c0d65e97f9c39a
Bartolomeo Piotti <piotti.bartolomeo@gmail.com>
2020-06-10 17:26:12 +0200

    Milestone I

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-    var ctx = document.getElementById('myChart').getContext('2d');
 
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 
-    var guadagni_per_mese = []
+    var guadagni_per_mese = [];
+    var guadagni_a_testa = {};
+    var guadagno_annuale = 0;
 
     $.ajax({
         'url': 'http://157.230.17.132:4018/sales',
@@ -15,20 +16,45 @@ $(document).ready(function() {
         'success': function(data) {
 
             for (var i = 0; i < data.length; i++) {
+
                 mese_data_corrente = moment(data[i].date, 'DD/MM/YYYY').format('M')
+
                 if (typeof guadagni_per_mese[mese_data_corrente - 1] == 'undefined') {
                     guadagni_per_mese[mese_data_corrente - 1] = 0;
                 }
 
                 guadagni_per_mese[mese_data_corrente - 1] += data[i].amount;
+
+                //Crea oggetto {Salesman: guadagno}
+                if (guadagni_a_testa.hasOwnProperty(data[i].salesman)) {
+                    guadagni_a_testa[data[i].salesman] += data[i].amount;
+                } else {
+                    guadagni_a_testa[data[i].salesman] = data[i].amount
+                }
+
+                //Somma tutti i guadagni per il totale annuale
+                guadagno_annuale += data[i].amount
             }
+
+            console.log(guadagni_a_testa);
+            console.log(guadagno_annuale);
             lineChart(months, guadagni_per_mese)
+
+            //Trasforma in percentuale i valori del guadagno a testa
+            for (var key in guadagni_a_testa) {
+                guadagni_a_testa[key] = (guadagni_a_testa[key] * 100 / guadagno_annuale).toFixed(1)
+            }
+
+            pieChart(Object.keys(guadagni_a_testa), Object.values(guadagni_a_testa))
 
         }
     })
 
+    //Grafico 1
+    var line_chart = document.getElementById('lineChart').getContext('2d');
+
     function lineChart(x, y) {
-        var config = new Chart(ctx, {
+        var config = new Chart(line_chart, {
             type: 'line',
             data: {
                 labels: x,
@@ -41,11 +67,38 @@ $(document).ready(function() {
                     lineTension: 0,
                 }],
             },//Data
-            // options: {
-            //     // This chart will not respond to mousemove, etc
-            //     events: ['click']
-            // }
-        }); //New chart
+        }); //config
+    }
+
+    //Grafico 2
+    var pie_chart = document.getElementById('pieChart').getContext('2d');
+
+    function pieChart(x, y) {
+        var config = new Chart(pie_chart, {
+            type: 'pie',
+            data: {
+                labels: x,
+                datasets: [{
+                    data: y,
+                    fill: true,
+                    backgroundColor: ['#f4f6ff', '#f3c623', '#127681', '#10375c'],
+                    hoverBackgroundColor: ['#f1f1fd'],
+                    borderWidth: 0,
+                }],
+            },//Data
+            options: {
+                    legend: {
+                        position: 'left'
+                    },
+                    title: {
+                        display: true,
+                        text: '% per salesman',
+                        position: 'left',
+                        fontSize: 30,
+                        fontStyle: 'normal',
+                    }
+            }
+        }); //config
     }
 
 })//Ready
